e3c9f9632d8dfad7466f9d3e5de2f3fa2ef7265b
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-09-21 17:10:11 +0200

    def cuadrante

--- ejercicio.py
+++ ejercicio.py
@@ -20,3 +20,27 @@ class Punto:
         cadena = "(" + str(self.cordX) + "," + str(self.cordY) + ")"
         return cadena
 
+    def cuadrante(self):
+        if self.cordX > 0:
+            if self.cordY > 0:
+                print("Tu punto está en el primer cuadrante")
+            elif self.cordY < 0:
+                print("Tu punto está en el cuarto cuadrante")
+            else:
+                print("Tu punto está sobre el eje x, a la derecha del origen.")
+        elif self.cordX < 0:
+            if self.cordY > 0:
+                print("Tu punto está en el segundo cuadrante")
+            elif self.cordY < 0:
+                print("Tu punto está en el tercer cuadrante")
+            else:
+                print("Tu punto está sobre el eje x, a la izquierda del origen")
+        else:
+            if self.cordY > 0:
+                print("Tu punto está sobre el eje y, por encima del origen")
+            elif self.cordY < 0:
+                print("Tu punto está sobre el eje y, por debajo del origen")
+            else:
+                print("Tu punto es el origen de coordenadas")
+
+            
