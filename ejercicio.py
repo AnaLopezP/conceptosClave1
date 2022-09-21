9dd330f02eebb916797456be085e39b4cd147839
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-09-21 16:53:18 +0200

    clase punto con constructor

---
+++ ejercicio.py
@@ -0,0 +1,18 @@
+from curses.ascii import isdigit
+
+
+class Punto:
+    def __init__(self, cordX, cordY):
+        self.cordX = cordX 
+        self.cordY = cordY
+        x = cordX.isdigit()
+        y = cordY.isdigit()
+        if x == False:
+            cordX = 0
+        else:
+            pass
+        if y == False:
+            cordY = 0
+        else:
+            pass
+
